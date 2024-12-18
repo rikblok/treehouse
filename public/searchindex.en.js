b399496408a2c2c388ddaca09e8bb56cb94daf57
UBC Student <rik.blok@ubc.ca>
2024-12-17 22:48:43 -0800

    add relearn theme as submodule

---
+++ public/searchindex.en.js
@@ -0,0 +1,42 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "My New Hugo Site \u003e Posts",
+    "content": "Introduction This is bold text, and this is emphasized text.\nVisit the Hugo website!",
+    "description": "Introduction This is bold text, and this is emphasized text.\nVisit the Hugo website!",
+    "tags": [],
+    "title": "My First Post",
+    "uri": "/posts/my-first-post/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "My New Hugo Site",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Posts",
+    "uri": "/posts/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "My New Hugo Site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
